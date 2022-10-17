f3a2bb0d88df6f094b42af9e543ca3d551b1fc00
PraveenKumarPanchadarla <panchadarla.paveenkumar4@gmail.com>
2022-10-17 20:43:21 +0530

    Updates

---
+++ precache-manifest.0bdd36ebaf4c850cf513d4876d178a08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1974c03017ef5ca48bc",
+    "url": "/Original-Six/static/css/main.28307e79.chunk.css"
+  },
+  {
+    "revision": "d1974c03017ef5ca48bc",
+    "url": "/Original-Six/static/js/main.d1974c03.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/Original-Six/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "44e11edd313d47118280",
+    "url": "/Original-Six/static/js/2.44e11edd.chunk.js"
+  },
+  {
+    "revision": "1d7f01eb8343cc407919",
+    "url": "/Original-Six/static/js/runtime~main.1d7f01eb.js"
+  },
+  {
+    "revision": "f79789e4554a40e971e4967ea6620d24",
+    "url": "/Original-Six/static/media/heading.f79789e4.jpg"
+  },
+  {
+    "revision": "ec917fe1d84ae9e060cba40154ac349a",
+    "url": "/Original-Six/static/media/image.ec917fe1.jpg"
+  },
+  {
+    "revision": "d356f95de71284db58d8c94edafe8b18",
+    "url": "/Original-Six/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Original-Six/precache-manifest.0bdd36ebaf4c850cf513d4876d178a08.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Original-Six/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
