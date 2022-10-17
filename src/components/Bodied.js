4b9dec33cbd8296427fb342c391c8ff8dac4e4e4
Praveen Kumar Panchadarla <panchadarla.praveenkumar4@gmail.com>
2022-10-17 20:50:03 +0530

    Update Bodied.js

--- src/components/Bodied.js
+++ src/components/Bodied.js
@@ -11,12 +11,12 @@ const Bodied= ({filter})=>{
             filter.map((item)=>(
                 <div className="container" key={item.id}>
                     <img src={item.pic1}  onClick = {() => setSelectedImg(item.pic1)}/>
-                    <img src={item.pic3}  onClick={() => setSelectedImg(item.pic2)}/>
-                    <img src={item.pic2}  onClick={() => setSelectedImg(item.pic3)}/>
+                    <img src={item.pic2}  onClick={() => setSelectedImg(item.pic2)}/>
+                    <img src={item.pic3}  onClick={() => setSelectedImg(item.pic3)}/>
                     <img src={item.pic4}  onClick={() => setSelectedImg(item.pic4)}/>
                     <img src={item.pic5}  onClick={() => setSelectedImg(item.pic5)}/>
-                    <img src={item.pic7}  onClick={() => setSelectedImg(item.pic6)}/>
-                    <img src={item.pic6}  onClick={() => setSelectedImg(item.pic7)}/>
+                    <img src={item.pic6}  onClick={() => setSelectedImg(item.pic6)}/>
+                    <img src={item.pic7}  onClick={() => setSelectedImg(item.pic7)}/>
                     <img src={item.pic8}  onClick={() => setSelectedImg(item.pic8)}/>
                     <img src={item.pic9}  onClick={() => setSelectedImg(item.pic9)}/>
                     <img src={item.pic10} onClick={() => setSelectedImg(item.pic10)}/>
